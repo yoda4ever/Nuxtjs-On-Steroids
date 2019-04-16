88f8e7834f79827c8b5d04629de05fdbb7eaba92
Leandro lima <leandrocrlima@gmail.com>
2019-04-16 17:25:56 -0300

    Error page added

---
+++ validation-project/layouts/error.vue
@@ -0,0 +1,7 @@
+<template>
+    <center>
+        <h1> Opps... this is embarrassing.</h1>
+        <h4>Please revise your operation and try again</h4>
+        <nuxt-link to="/users">users</nuxt-link>
+    </center>
+</template>
